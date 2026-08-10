870c85410bd5a8ec82f8302bce154a8882877cfb
Christopher Qiao <40278799+cqiao64@users.noreply.github.com>
2026-08-09 20:58:56 -0400

    Add: sql-calculate-special-bonus - submission-0

---
+++ SQL/sql-calculate-special-bonus/submission-0.sql
@@ -0,0 +1,11 @@
+-- Write your query below
+
+select 
+    employee_id,
+    CASE
+        WHEN employee_id % 2 = 1 and name not like 'M%' THEN salary
+        else 0
+    end as bonus
+from employees
+order by employee_id;
+
